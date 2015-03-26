a97c3aeafb5e4531b04cf879d4a72873dec65a84
Tim Leland <timothyl88@gmail.com>
2015-03-25 22:27:58 -0400

    fix codes

--- toggle.php
+++ toggle.php
@@ -4,29 +4,29 @@ $outletLight = $_POST['outletId'];
 $outletStatus = $_POST['outletStatus'];
 
 if ($outletLight == "1" && $outletStatus == "on") {
-    $rfCodes = array(1070387);
+    $rfCodes = array(349491);
 } else if ($outletLight == "1" && $outletStatus == "off") {
-    $rfCodes = array(1070396);
+    $rfCodes = array(349500);
 } else if ($outletLight == "2" && $outletStatus == "on") {
-    $rfCodes = array(1070531);
+    $rfCodes = array(349635);
 } else if ($outletLight == "2" && $outletStatus == "off") {
-    $rfCodes = array(1070540);
+    $rfCodes = array(349644);
 } else if ($outletLight == "3" && $outletStatus == "on") {
-    $rfCodes = array(1070851);
+    $rfCodes = array(349955);
 } else if ($outletLight == "3" && $outletStatus == "off") {
-    $rfCodes = array(1070860);
+    $rfCodes = array(349964);
 } else if ($outletLight == "4" && $outletStatus == "on") {
-    $rfCodes = array(1072387);
+    $rfCodes = array(351491);
 } else if ($outletLight == "4" && $outletStatus == "off") {
-    $rfCodes = array(1072396);
+    $rfCodes = array(351500);
 } else if ($outletLight == "5" && $outletStatus == "on") {
-    $rfCodes = array(1078531);
+    $rfCodes = array(357635);
 } else if ($outletLight == "5" && $outletStatus == "off") {
-    $rfCodes = array(1078540);
+    $rfCodes = array(357644);
 } else if ($outletLight == "6" && $outletStatus == "on") {
-    $rfCodes = array(1070387, 1070531, 1070851, 1072387, 1078531);
+    $rfCodes = array(349491, 349635, 349955, 351491, 357635);
 } else if ($outletLight == "6" && $outletStatus == "off") {
-        $rfCodes = array(1070396, 1070540, 1070860, 1072396, 1078540);
+        $rfCodes = array(349500, 349644, 349964, 351500, 357644);
 }
 
 
