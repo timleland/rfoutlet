2deec8da1e9371a92277f0ff99e3d51aee1cc8ee
Marc Etcheverry <marc@taplightsoftware.com>
2017-10-15 12:15:36 -0700

    Update RCSwitch, update Makefile and add our own sniffer

--- RFSource/RCSwitch.h
+++ RFSource/RCSwitch.h
@@ -1,12 +1,17 @@
 /*
   RCSwitch - Arduino libary for remote control outlet switches
-  Copyright (c) 2011 Suat Özgür.  All right reserved.
+  Copyright (c) 2011 Suat Ã–zgÃ¼r.  All right reserved.
 
   Contributors:
   - Andre Koehler / info(at)tomate-online(dot)de
   - Gordeev Andrey Vladimirovich / gordeev(at)openpyro(dot)com
+  - Skineffect / http://forum.ardumote.com/viewtopic.php?f=2&t=46
+  - Dominik Fischer / dom_fischer(at)web(dot)de
+  - Frank Oltmanns / <first name>.<last name>(at)gmail(dot)com
+  - Max Horn / max(at)quendi(dot)de
+  - Robert ter Vehn / <first name>.<last name>(at)gmail(dot)com
   
-  Project home: http://code.google.com/p/rc-switch/
+  Project home: https://github.com/sui77/rc-switch/
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
@@ -27,29 +32,33 @@
 
 #if defined(ARDUINO) && ARDUINO >= 100
     #include "Arduino.h"
-#else
+#elif defined(ENERGIA) // LaunchPad, FraunchPad and StellarPad specific
+    #include "Energia.h"
+#elif defined(RPI) // Raspberry Pi
+    #define RaspberryPi
+
+    // Include libraries for RPi:
+    #include <string.h> /* memcpy */
+    #include <stdlib.h> /* abs */
     #include <wiringPi.h>
-    #include <stdint.h>
-    #define NULL 0
-    #define CHANGE 1
-#ifdef __cplusplus
-extern "C"{
-#endif
-typedef uint8_t boolean;
-typedef uint8_t byte;
-
-#if !defined(NULL)
-#endif
-#ifdef __cplusplus
-}
-#endif
+#elif defined(SPARK)
+    #include "application.h"
+#else
+    #include "WProgram.h"
 #endif
 
+#include <stdint.h>
+
+
+// At least for the ATTiny X4/X5, receiving has to be disabled due to
+// missing libm depencies (udivmodhi4)
+#if defined( __AVR_ATtinyX5__ ) or defined ( __AVR_ATtinyX4__ )
+#define RCSwitchDisableReceiving
+#endif
 
 // Number of maximum High/Low changes per packet.
 // We can handle up to (unsigned long) => 32 bit * 2 H/L changes per bit + 2 for sync
 #define RCSWITCH_MAX_CHANGES 67
-
 
 class RCSwitch {
 
@@ -58,64 +67,88 @@ class RCSwitch {
     
     void switchOn(int nGroupNumber, int nSwitchNumber);
     void switchOff(int nGroupNumber, int nSwitchNumber);
-    void switchOn(char* sGroup, int nSwitchNumber);
-    void switchOff(char* sGroup, int nSwitchNumber);
+    void switchOn(const char* sGroup, int nSwitchNumber);
+    void switchOff(const char* sGroup, int nSwitchNumber);
     void switchOn(char sFamily, int nGroup, int nDevice);
     void switchOff(char sFamily, int nGroup, int nDevice);
+    void switchOn(const char* sGroup, const char* sDevice);
+    void switchOff(const char* sGroup, const char* sDevice);
+    void switchOn(char sGroup, int nDevice);
+    void switchOff(char sGroup, int nDevice);
 
-    void sendTriState(char* Code);
-    void send(unsigned long Code, unsigned int length);
-    void send(char* Code);
+    void sendTriState(const char* sCodeWord);
+    void send(unsigned long code, unsigned int length);
+    void send(const char* sCodeWord);
     
+    #if not defined( RCSwitchDisableReceiving )
     void enableReceive(int interrupt);
     void enableReceive();
     void disableReceive();
     bool available();
-	void resetAvailable();
-	
+    void resetAvailable();
+
     unsigned long getReceivedValue();
     unsigned int getReceivedBitlength();
     unsigned int getReceivedDelay();
-	unsigned int getReceivedProtocol();
+    unsigned int getReceivedProtocol();
     unsigned int* getReceivedRawdata();
+    #endif
   
     void enableTransmit(int nTransmitterPin);
     void disableTransmit();
     void setPulseLength(int nPulseLength);
     void setRepeatTransmit(int nRepeatTransmit);
+    #if not defined( RCSwitchDisableReceiving )
     void setReceiveTolerance(int nPercent);
-	void setProtocol(int nProtocol);
-	void setProtocol(int nProtocol, int nPulseLength);
-  
+    #endif
+
+    struct HighLow {
+        uint8_t high;
+        uint8_t low;
+    };
+
+    struct Protocol {
+        int pulseLength;
+        HighLow syncFactor;
+        HighLow zero;
+        HighLow one;
+        /** @brief if true inverts the high and low logic levels in the HighLow structs */
+        bool invertedSignal;
+    };
+
+    void setProtocol(Protocol protocol);
+    void setProtocol(int nProtocol);
+    void setProtocol(int nProtocol, int nPulseLength);
+
   private:
-    char* getCodeWordB(int nGroupNumber, int nSwitchNumber, boolean bStatus);
-    char* getCodeWordA(char* sGroup, int nSwitchNumber, boolean bStatus);
-    char* getCodeWordC(char sFamily, int nGroup, int nDevice, boolean bStatus);
-    void sendT0();
-    void sendT1();
-    void sendTF();
-    void send0();
-    void send1();
-    void sendSync();
-    void transmit(int nHighPulses, int nLowPulses);
+    char* getCodeWordA(const char* sGroup, const char* sDevice, bool bStatus);
+    char* getCodeWordB(int nGroupNumber, int nSwitchNumber, bool bStatus);
+    char* getCodeWordC(char sFamily, int nGroup, int nDevice, bool bStatus);
+    char* getCodeWordD(char group, int nDevice, bool bStatus);
+    void transmit(HighLow pulses);
 
-    static char* dec2binWzerofill(unsigned long dec, unsigned int length);
-    
+    #if not defined( RCSwitchDisableReceiving )
     static void handleInterrupt();
-	static bool receiveProtocol1(unsigned int changeCount);
-	static bool receiveProtocol2(unsigned int changeCount);
+    static bool receiveProtocol(const int p, unsigned int changeCount);
     int nReceiverInterrupt;
+    #endif
     int nTransmitterPin;
-    int nPulseLength;
     int nRepeatTransmit;
-	char nProtocol;
+    
+    Protocol protocol;
 
-	static int nReceiveTolerance;
+    #if not defined( RCSwitchDisableReceiving )
+    static int nReceiveTolerance;
     static unsigned long nReceivedValue;
     static unsigned int nReceivedBitlength;
-	static unsigned int nReceivedDelay;
-	static unsigned int nReceivedProtocol;
+    static unsigned int nReceivedDelay;
+    static unsigned int nReceivedProtocol;
+    const static unsigned int nSeparationLimit;
+    /* 
+     * timings[0] contains sync timing, followed by a number of bits
+     */
     static unsigned int timings[RCSWITCH_MAX_CHANGES];
+    #endif
 
     
 };
